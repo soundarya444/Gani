7e9a68b27aee7829108de5be723dd21e7fdc3db4
Rahumangani <42862312+Rahumangani@users.noreply.github.com>
2019-02-07 15:04:03 +0530

    sixth.py

--- sixth.py
+++ sixth.py
@@ -1,6 +1,6 @@
-year = int(input())
+Year = int(input())
 
-if (( year%400 == 0)or (( year%4 == 0 ) and ( year%100 != 0))):
+if (( Year%400 == 0)or (( Year%4 == 0 ) and ( Year%100 != 0))):
     print("yes")
 else:
     print("no")
