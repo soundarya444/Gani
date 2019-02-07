ff3aa16db51eef2e22cb3df1d6460efb0d271d0d
Rahumangani <42862312+Rahumangani@users.noreply.github.com>
2019-02-07 14:39:33 +0530

    fifth.py

--- fifth.py
+++ fifth.py
@@ -1,10 +1,13 @@
-num1=int(input("enter the first  number:"))
-num2=int(input("enter the second number:"))
-num3=int(input("enter the third number:"))
-if (num1 >= num2) and (num1 >= num3):
+aaa=input()
+aaa=aaa.split()
+num1=int(aaa[0])
+num2=int(aaa[1])
+num3=int(aaa[2])
+if (num1 >= num2) and (num1 > num3):
    largest = num1
-elif (num2 >= num1) and (num2 >= num3):
+elif (num2 >= num1) and (num2 > num3):
    largest = num2
 else:
    largest = num3
+ 
 print(largest)
